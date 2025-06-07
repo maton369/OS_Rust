cf40d7bb31b03011a6aef130ae47b11ae8ee8a46
maton369 <ysk901085@gmail.com>
2025-06-07 13:45:57 +0900

    Add: QEMUで無限ループを実行

--- etc.sh
+++ etc.sh
@@ -1,3 +1,9 @@
-curl -s https://hikalium.com/bmp/white_diamond_5x5.bmp.hex > bmp.hex
-cat bmp.hex | sed 's/ffffff/ff0000/g' | sed 's/000000/00ffff/g' > bmp2.hex
-cat bmp2.hex | xxd -r -p > bmp2.bmp
+#!/bin/zsh
+
+# curl -s https://hikalium.com/bmp/white_diamond_5x5.bmp.hex > bmp.hex
+# cat bmp.hex | sed 's/ffffff/ff0000/g' | sed 's/000000/00ffff/g' > bmp2.hex
+# cat bmp2.hex | xxd -r -p > bmp2.bmp
+
+# xxd -r -p img.hex > img.bin
+# ls -lah img.bin
+qemu-system-x86_64 -drive file=img.bin,format=raw
