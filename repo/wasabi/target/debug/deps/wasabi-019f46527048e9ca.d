5678f0fdd9f82cef45df49f67df415e3b30b9523
maton369 <ysk901085@gmail.com>
2025-06-09 12:16:26 +0900

    Add: ホスト環境でのRustの挙動を確認

---
+++ repo/wasabi/target/debug/deps/wasabi-019f46527048e9ca.d
@@ -0,0 +1,5 @@
+/Users/maton/OS_Rust/repo/wasabi/target/debug/deps/wasabi-019f46527048e9ca: src/main.rs
+
+/Users/maton/OS_Rust/repo/wasabi/target/debug/deps/wasabi-019f46527048e9ca.d: src/main.rs
+
+src/main.rs:
---
+++ repo/wasabi/target/debug/wasabi.d
@@ -0,0 +1,1 @@
+/Users/maton/OS_Rust/repo/wasabi/target/debug/wasabi: /Users/maton/OS_Rust/repo/wasabi/src/main.rs
