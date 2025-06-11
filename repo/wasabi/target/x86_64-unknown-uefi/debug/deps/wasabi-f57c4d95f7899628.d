2b4e17512008c8d537162afe42cd6a59264aa5a1
maton369 <ysk901085@gmail.com>
2025-06-11 20:24:24 +0900

    Add: xHCに関係する値をレジスタから表示

--- repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d
+++ repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d
@@ -1,14 +1,16 @@
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-f57c4d95f7899628.rmeta: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/xhci.rs src/test_runner.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-f57c4d95f7899628.rmeta: src/lib.rs src/acpi.rs src/allocator.rs src/bits.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mmio.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/volatile.rs src/x86.rs src/xhci.rs src/test_runner.rs src/tests.rs src/./font.txt
 
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/xhci.rs src/test_runner.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d: src/lib.rs src/acpi.rs src/allocator.rs src/bits.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mmio.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/volatile.rs src/x86.rs src/xhci.rs src/test_runner.rs src/tests.rs src/./font.txt
 
 src/lib.rs:
 src/acpi.rs:
 src/allocator.rs:
+src/bits.rs:
 src/executor.rs:
 src/graphics.rs:
 src/hpet.rs:
 src/init.rs:
+src/mmio.rs:
 src/mutex.rs:
 src/pci.rs:
 src/print.rs:
@@ -16,6 +18,7 @@ src/qemu.rs:
 src/result.rs:
 src/serial.rs:
 src/uefi.rs:
+src/volatile.rs:
 src/x86.rs:
 src/xhci.rs:
 src/test_runner.rs:
