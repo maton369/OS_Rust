aeb6c01dc23422e1dc29d33c6a1957b98ea9539a
maton369 <ysk901085@gmail.com>
2025-06-11 16:43:13 +0900

    Add: PCIのベンダーIDとデバイスIDを表示

--- repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-656e9eb1d00d0147.d
+++ repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-656e9eb1d00d0147.d
@@ -1,8 +1,8 @@
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-656e9eb1d00d0147.rmeta: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-656e9eb1d00d0147.rmeta: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/tests.rs src/./font.txt
 
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-656e9eb1d00d0147.rlib: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-656e9eb1d00d0147.rlib: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/tests.rs src/./font.txt
 
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-656e9eb1d00d0147.d: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-656e9eb1d00d0147.d: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/tests.rs src/./font.txt
 
 src/lib.rs:
 src/acpi.rs:
@@ -12,6 +12,7 @@ src/graphics.rs:
 src/hpet.rs:
 src/init.rs:
 src/mutex.rs:
+src/pci.rs:
 src/print.rs:
 src/qemu.rs:
 src/result.rs:
--- repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d
+++ repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d
@@ -1,6 +1,6 @@
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-f57c4d95f7899628.rmeta: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/test_runner.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-f57c4d95f7899628.rmeta: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/test_runner.rs src/tests.rs src/./font.txt
 
-/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/test_runner.rs src/tests.rs src/./font.txt
+/Users/maton/OS_Rust/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-f57c4d95f7899628.d: src/lib.rs src/acpi.rs src/allocator.rs src/executor.rs src/graphics.rs src/hpet.rs src/init.rs src/mutex.rs src/pci.rs src/print.rs src/qemu.rs src/result.rs src/serial.rs src/uefi.rs src/x86.rs src/test_runner.rs src/tests.rs src/./font.txt
 
 src/lib.rs:
 src/acpi.rs:
@@ -10,6 +10,7 @@ src/graphics.rs:
 src/hpet.rs:
 src/init.rs:
 src/mutex.rs:
+src/pci.rs:
 src/print.rs:
 src/qemu.rs:
 src/result.rs:
